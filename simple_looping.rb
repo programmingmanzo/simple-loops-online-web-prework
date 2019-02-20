f41703380de80f48ddddfdc1dfcdec462acb72e1
Gerardo Manzo <programmingmanzo@gmail.com>
2019-02-20 06:32:03 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -10,7 +10,8 @@ def loop_iterator(number_of_times)
 end
 
 def times_iterator(number_of_times)
-  
+  (number_of_times).times do 
+    
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
