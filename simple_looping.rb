49789b377f8649867211c1d527158a7ea800bb48
Gerardo Manzo <programmingmanzo@gmail.com>
2019-02-20 06:28:11 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -3,7 +3,7 @@
 def loop_iterator(number_of_times)
   counter = 0 
   loop do 
-    puts "Welcome to Flatiron School's Web Development Course"
+    puts "Welcome to Flatiron School's Web Development Course!"
     counter += 1 
     break if counter == number_of_times
   end 
